d4ca026373cbcebd92f61010899715494c72a471
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-12-13 18:43:22 +0300

    [ADDED]: shubham This code for print Incre/Decrement Operation Demo

---
+++ IncreDecrementDemo1.php
@@ -0,0 +1,24 @@
+<?php
+
+	$a=10;
+	$b=20;
+
+
+	echo"Before Increament<br>";
+	echo"A=$a<br>";
+	
+	$a++;
+	
+	echo"After Increament<br>";
+	echo"A=$a<br>";
+
+	echo"Before Decreament<br>";
+	echo"B=$b<br>";
+	
+	$b--;
+	
+	echo"After Decreament<br>";
+	echo"B=$b<br>";
+
+
+?>
