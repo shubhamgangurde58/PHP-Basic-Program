6949086a924a1c437e7671e3303d61ec03336b16
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-12-13 18:40:54 +0300

    [ADDED]: shubham This code for print  Relational Operator Operation

---
+++ RelationalOperatorDemo1.php
@@ -0,0 +1,19 @@
+<?php
+
+	$a=10;
+	$b=10;
+
+	var_dump($a<$b);
+	echo"<br>";
+	var_dump($a<=$b);
+	echo"<br>";
+	var_dump($a>$b);
+	echo"<br>";
+	var_dump($a>=$b);
+	echo"<br>";
+	var_dump($a==$b);
+	echo"<br>";
+	var_dump($a!=$b);
+
+
+?>
