cae08d4148dc008fc6c3b0c326f80d43342775df
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-12-13 18:39:17 +0300

    [ADDED]: shubham This code for print Arithmetic operator Operation

---
+++ ArithmeticOperatorDemo1.php
@@ -0,0 +1,15 @@
+<?php
+
+    $a=12;
+    $b=5;
+
+    $add=$a+$b;
+    $sub=$a-$b;
+    $multi=$a*$b;
+    $divi=$a/$b;
+
+    echo"Addition=$add<br>";
+    echo"Subtraction=$sub<br>";
+    echo"Multiple=$multi<br>";
+    echo"Division=$divi<br>";
+?>
