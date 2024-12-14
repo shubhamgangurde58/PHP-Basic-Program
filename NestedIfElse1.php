a107f5141d7511ef6fcee04a80ac54a07e54c985
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-12-14 10:53:38 +0300

    [ADDED]: shubham This code for print Nested if else demo

---
+++ NestedIfElse1.php
@@ -0,0 +1,32 @@
+<?php
+
+	$a=40;
+	$b=50;
+	$c=30;
+
+	if($a > $b){
+
+		if($a > $b){
+
+			echo"A is Big";
+
+		}else{
+
+			echo"B is Big";
+		}
+
+	}else{
+
+
+		if($b > $c){
+
+			echo"B is Big";
+
+		}else{
+
+			echo"C is Big";
+		}
+
+	}
+	
+?>
